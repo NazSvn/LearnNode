53cfe0b20c050e3c3c4ad1a95b2de6eb7966f3f0
NazSvn <idler90@gmail.com>
2025-01-23 22:04:36 +0800

    feat: setup initial authentication routes
    - Add login, register, logout, and protected routes
    - Configure Express server with JSON middleware
    - Implement basic user authentication structure

---
+++ User_Auth_Reg/index.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import { PORT } from './config.js'
+import { UserRepository } from './user-repository.js'
+
+const app = express()
+
+app.disable('x-powered-by')
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.send('hello')
+})
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await UserRepository.login({ username, password })
+    res.status(200).json({ message: 'Login successful', user: user.username })
+  } catch (error) {
+    console.error(error)
+    res.status(401).json({ message: error.message })
+  }
+})
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const id = await UserRepository.create({ username, password })
+    res.status(201).json({ message: 'User created successfully', userId: id })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: error.message })
+  }
+})
+app.post('/logout', async (req, res) => {})
+
+app.get('/protected', async (req, res) => {})
+
+app.listen(PORT, () => {
+  console.log(`Server is runnning on http://localhost:${PORT}`)
+})
