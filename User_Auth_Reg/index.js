dd24d9560f23caa451a9a1e53a3bc033c85e7592
NazSvn <idler90@gmail.com>
2025-01-28 21:25:12 +0800

    feat: implement JWT authentication and session handling
    - Add JWT token generation and verification
    - Implement secure cookie-based token storage
    - Add middleware to check user session
    - Configure cookie security settings for production
    - Add authentication check for protected routes
    - Implement logout functionality to clear token

--- User_Auth_Reg/index.js
+++ User_Auth_Reg/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { PORT } from './config.js'
+import { JWT_KEY, PORT } from './config.js'
 import { UserRepository } from './user-repository.js'
 import cors from 'cors'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
@@ -9,9 +11,24 @@ app.set('view engine', 'ejs')
 app.use(cors())
 app.disable('x-powered-by')
 app.use(express.json())
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  req.session = { user: null }
+
+  try {
+    const decoded = jwt.verify(token, JWT_KEY)
+    req.session.user = decoded
+  } catch {
+    /* Empty */
+  }
+  next()
+})
 
 app.get('/', async (req, res) => {
-  res.render('index')
+  const user = req.session.user
+  res.render('index', user)
 })
 
 app.get('/user-login', async (req, res) => {
@@ -23,7 +40,19 @@ app.post('/login', async (req, res) => {
 
   try {
     const user = await UserRepository.login({ username, password })
-    res.status(200).json({ message: 'Login successful', user: user.username })
+    const token = jwt.sign({ id: user.id, username: user.username }, JWT_KEY, {
+      expiresIn: '1h'
+    })
+
+    res
+      .cookie('access_token', token, {
+        httpOnly: true, // Cannot be accessed by JavaScript
+        secure: process.env.NODE_ENV === 'production', // Can only be sent over HTTPS
+        sameSite: 'strict', // Can only be sent to the same site
+        maxAge: 1000 * 60 * 60 // Expires in 1 hour
+      })
+      .status(200)
+      .json({ message: 'Login successful', user: user.username })
   } catch (error) {
     console.error(error)
     res.status(401).json({ message: error.message })
@@ -44,9 +73,16 @@ app.post('/register', async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 })
-app.post('/logout', async (req, res) => {})
+app.post('/logout', async (req, res) => {
+  res.clearCookie('access_token').json({ message: 'Logged out' })
+})
 
-app.get('/protected', async (req, res) => {})
+app.get('/protected', async (req, res) => {
+  const user = req.session.user
+  if (!user) return res.status(401).json({ message: 'Unauthorized' })
+
+  res.render('protected', user)
+})
 
 app.listen(PORT, () => {
   console.log(`Server is runnning on http://localhost:${PORT}`)
